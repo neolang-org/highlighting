d925ff1042b9065dcea40b0adb0aefd81b765d53
Skyler Burwell <skburwell@proton.me>
2026-03-23 22:36:12 -0600

    Fix indent problem.

--- zed/neo-zed/languages/neo/indents.scm
+++ zed/neo-zed/languages/neo/indents.scm
@@ -1,23 +1,5 @@
-; Indent on open braces/parens/brackets
-[
-  (block)
-  (struct_definition)
-  (enum_definition)
-  (union_definition)
-  (error_definition)
-  (when_statement)
-  (when_expression)
-  (parameter_list)
-  (array_init_expression)
-  (struct_init_expression)
-  (map_init_expression)
-  (extern_block)
-  (anonymous_struct_type)
-] @indent
+; Indent after opening brace
+(_ "{" @indent "}" @end)
 
-; Dedent on close braces
-[
-  "}"
-  ")"
-  "]"
-] @outdent
+; Dedent on closing brace
+"}" @outdent
